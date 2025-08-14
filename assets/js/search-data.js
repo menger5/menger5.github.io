89501003b4fbd2008cdf17f7231b2c2d35c7c7d8
menger5 <143822465+menger5@users.noreply.github.com>
2025-08-14 00:38:23 +0000

    Deploying to gh-pages from @ menger5/menger5.github.io@7c6d9b8772d6e8df8c76d60451afcc5e881ea665 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-bookshelf",
-          title: "bookshelf",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/books/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
@@ -50,6 +43,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-bookshelf",
+          title: "bookshelf",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
           },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
